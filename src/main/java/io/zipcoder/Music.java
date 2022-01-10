5bc78c0f9afa7f0082c31046b82a7cd412c0ed8f
That is me <trab.die@gmail.com>
2022-01-10 15:08:35 -0500

    init and final commit

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,47 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        // left count initialisation
+        int lCount=0;
+
+        // right count initialisation
+        int rCount=0;
+
+        //counting from the right
+        int k=startIndex;
+        while(playList[k]!=selection){
+            k++;
+            rCount++;
+            if(k==playList.length){
+                k=0;
+            }
+
+        }
+
+        //counting from the left
+        int m=startIndex;
+        while(playList[m]!=selection){
+            m--;
+            lCount++;
+
+            if(m<0){
+                m=playList.length-1;
+            }
+
+        }
+
+        if(lCount<=rCount){
+            return lCount;
+        }
+
+        return rCount;
     }
+
+
 }
